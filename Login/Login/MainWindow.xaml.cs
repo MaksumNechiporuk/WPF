c09528c3979642f2e7e3f8eebd12f276131e7d61
MaksumNeciporuk <maksum.nec@gmail.com>
2019-05-20 12:01:24 +0300

    add project

---
+++ Login/Login/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Login
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        OpenFileDialog myDialog = new OpenFileDialog();
+        List<Person> people = new List<Person>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void Img_MouseEnter(object sender, MouseEventArgs e)
+        {
+
+            try
+            {
+                OpenFileDialog dlg = new OpenFileDialog();
+                dlg.Title = "Select a picture";
+                dlg.Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" +
+                  "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
+                  "Portable Network Graphic (*.png)|*.png";
+
+                if   (dlg.ShowDialog()==true)
+                {
+                    img.Source = new BitmapImage(new Uri(dlg.FileName));
+
+                }
+
+            }
+            catch
+            {
+                MessageBox.Show("asdas");
+
+            }
+        }
+
+        private void BrdSiteLogo_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            people.Add(new Person(txtFirstName.Text, txtSecondName.Text, txtNumber.Text, img, txtCompany.Text, txtAdres.Text, txtEmail.Text, txtPassw.ToString()));
+        }
+    }
+}
---
+++ Login/Login/Person.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Login
+{
+   public class Person
+    {
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+
+        public string Number { get; set; }
+        public Image img { get; set; }
+
+        public string Company { get; set; }
+        public string Adress { get; set; }
+
+        public string Email { get; set; }
+
+        public string Passw { get; set; }
+        public Person(string firstN,string SecondN,string number,Image i,string company,string adress,string email,string passwd)
+        {
+            FirstName = firstN;
+            SecondName = SecondN;
+            Number = number;
+            img = i;
+            Company = company;
+            Adress = adress;
+            Email = email;
+             Passw = passwd;
+        }
+    }
+}
